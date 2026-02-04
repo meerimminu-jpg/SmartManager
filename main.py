b83168c8e1ed2e7450b1bf3ca5f82016d311c7fa
Meerim <@email.com>
2026-02-04 12:40:06 +0600

    Календарь функциясынын алгачкы коду кошулду

--- main.py
+++ main.py
@@ -1,32 +1,7 @@
-import os
-from datetime import datetime
-from colorama import Fore, Style, init
-from art import tprint
-
-# Түстөрдү жана режимдерди иштетүү
-init()
-
-def main():
-    # 1. Кооз логотип чыгаруу
-    print(Fore.YELLOW)
-    tprint("SmartManager") 
-    print(Style.RESET_ALL)
-
-    # 2. Системалык маалыматтарды алуу (Стандарттык китепканалар менен)
-    uibaqyt = datetime.now().strftime("%H:%M:%S")
-    papka = os.getcwd()
-
-    print(Fore.CYAN + "--- СИСТЕМАЛЫК МААЛЫМАТ ---")
-    print(f"Учурдагы убакыт: {uibaqyt}")
-    print(f"Долбоордун жайгашкан жери: {papka}")
-    print("-" * 27 + Style.RESET_ALL)
-
-    # 3. Колдонуучу менен интерактивдүү байланыш
-    print(Fore.GREEN + "\nПрограмма ийгиликтүү иштеп жатат!")
-    user_name = input(Fore.WHITE + "Студенттин аты-жөнү: ")
-    
-    print(f"\n{Fore.GREEN}Азаматсыз, {Fore.YELLOW}{user_name}{Fore.GREEN}!")
-    print(f"Сиз №4 лабораторияда 4 башка китепкананы колдондуңуз!{Style.RESET_ALL}")
+def show_calendar():
+    print("\n--- SmartManager Календары ---")
+    print("Бүгүнкү дата: 2024-жыл, 22-май")
+    print("Жакынкы иш-чаралар: Лабораториялык ишти тапшыруу.")
 
 if __name__ == "__main__":
-    main()
+    show_calendar()
