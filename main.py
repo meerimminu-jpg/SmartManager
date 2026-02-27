1556dda6b09637d06e8e2cd8f2ec997c2a17be68
Meerim <aasylbek@email.com>
2026-02-27 10:39:06 +0600

    Конфликт толугу менен тазаланды

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
 import datetime
 
 def show_time():
-<<<<<<< HEAD
     print("Негизги бутактын жаңы жазуусу")
-=======
-    print("Бул бутактын версиясы")
->>>>>>> feature-conflict
-    print(f"Учурдагы убакыт: {now.strftime('%H:%M:%S')}")
-=======
+    print(f"Учурдагы убакыт: {datetime.datetime.now().strftime('%H:%M:%S')}")
+
 def show_author():
     print("Программанын автору: Сиздин Атыңыз")
->>>>>>> feature-author
