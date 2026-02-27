a95fa4f6cd5a4f734ce98dc24980efa1752a83a6
Meerim <aasylbek@email.com>
2026-02-27 09:48:05 +0600

    Календарь функциясынын алгачкы коду кошулду

---
+++ main.py
@@ -0,0 +1,7 @@
+def show_calendar():
+    print("\n--- SmartManager Календары ---")
+    print("Бүгүнкү дата: 2024-жыл, 22-май")
+    print("Жакынкы иш-чаралар: Лабораториялык ишти тапшыруу.")
+
+if __name__ == "__main__":
+    show_calendar()
