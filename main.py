8e872b4fbd350c80a71f9a18c284adea8ed37b69
Meerim <aasylbek@email.com>
2026-02-27 10:06:39 +0600

    Автор жөнүндө маалымат кошулду

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
-def show_calendar():
-    print("\n--- SmartManager Календары ---")
-    print("Бүгүнкү дата: 2024-жыл, 22-май")
-    print("Жакынкы иш-чаралар: Лабораториялык ишти тапшыруу.")
-
-if __name__ == "__main__":
-    show_calendar()
+def show_author():
+    print("Программанын автору: Сиздин Атыңыз")
