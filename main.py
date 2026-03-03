b3de332a019aa16af14bd6979715096ee18e5f27
Meerim <aasylbek@email.com>
2026-03-03 08:18:03 +0600

    №7 лаборатория: try-except блогу кошулду

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-import datetime
+def add(a, b):
+    return a + b
 
-def show_time():
-    print("Негизги бутактын жаңы жазуусу")
-    print(f"Учурдагы убакыт: {datetime.datetime.now().strftime('%H:%M:%S')}")
+# test_calculator.py
+import pytest
+# Төмөнкү сапты өчүрдүк, себеби add() ушул эле файлда
+# from calculator import add 
 
-def show_author():
-    print("Программанын автору: Сиздин Атыңыз")
+def test_add_success():
+    assert add(2, 3) == 5
+
+def test_add_wrong():
+    assert add(2, 2) != 5
+
+def get_user_age():
+    try:
+        age = int(input("Жашыңызды киргизиңиз: "))
+        print(f"Сиздин жашыңыз: {age}")
+    except ValueError:
+        print("Ката! Сураныч, тамга эмес, бир гана сан киргизиңиз.")
+
+# Функцияны иштетүү үчүн аны чакыруу керек:
+if __name__ == "__main__":
+    get_user_age()
